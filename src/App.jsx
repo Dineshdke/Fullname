b1358f0c6bb449e54883a9b648974ac7ef4a0d90
Dinesh Kumar E <egdedk@gmail.com>
2024-05-07 18:49:37 +0530

    Finalised

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,9 @@ function App() {
   const [submit, setSubmit] = useState(false);
   const [first,setFirst] = useState('');
   const [last,setLast] = useState('');
-  // const [formError, setFormError] = useState({});
 
   const handleChange = (e) => {
     const {name,value} = e.target;
-    // setInitial({...initial, [name]: value});
     if(name=='first'){
       setFirst(value)
     }
@@ -24,22 +22,10 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // setFormError(validate({first,last}));
     if(first&&last){
       setSubmit(true);
     }
   }
-
-  // const validate = ({first,last})=>{
-  //   const error={};
-  //   if(!first){
-  //     error.first = 'Please input the First name';
-  //   }    
-  //   if(!last){
-  //     error.last = 'Please input the Last name';
-  //   }
-  //   return error;
-  // }
 
   return (
     <>
@@ -48,27 +34,22 @@ function App() {
         <label>
           First Name:<input type='text' name='first' value={first} onChange={handleChange} required/>
         </label>
-        {/* <div>
-          {formError.first} 
-        </div>        */}
         <div>
           Last Name:<input type='text' name='last' value={last} onChange={handleChange} required/>
         </div>
-        {/* <div>
-          {formError.last}
-        </div>      */}
         <br/>
         <button type="submit">Submit</button>
         <br/>
         <br/>
-        {submit ? (
-          <div>
-            Full Name: {first} {last}
-          </div>
+
+      </form>
+      {submit ? (
+          
+           `Full Name: ${first} ${last}`
+          
         ) : (
           ""
         )}
-      </form>
     </>
   );
 }
