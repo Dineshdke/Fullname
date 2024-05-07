b81508b6eed94c29e4de5f459bb19f89141cbc4a
Dinesh Kumar E <egdedk@gmail.com>
2024-05-07 18:54:53 +0530

    NEwFinal

--- src/App.jsx
+++ src/App.jsx
@@ -2,54 +2,65 @@ import { useState } from "react";
 // import './App.css'
 
 function App() {
-
   const [submit, setSubmit] = useState(false);
-  const [first,setFirst] = useState('');
-  const [last,setLast] = useState('');
+  const [first, setFirst] = useState("");
+  const [last, setLast] = useState("");
 
   const handleChange = (e) => {
-    const {name,value} = e.target;
-    if(name=='first'){
-      setFirst(value)
+    const { name, value } = e.target;
+    if (name == "first") {
+      setFirst(value);
+    } else {
+      setLast(value);
     }
-    else{
-      setLast(value)
-    }
-    if(first===''||last===''){
+    if (first === "" || last === "") {
       setSubmit(false);
     }
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if(first&&last){
+    if (first && last) {
       setSubmit(true);
     }
-  }
+  };
 
   return (
     <>
       <h1>Full Name Display</h1>
       <form onSubmit={handleSubmit}>
         <label>
-          First Name:<input type='text' name='first' value={first} onChange={handleChange} required/>
+          First Name:
+          <input
+            type="text"
+            name="first"
+            value={first}
+            onChange={handleChange}
+            required
+          />
         </label>
         <div>
-          Last Name:<input type='text' name='last' value={last} onChange={handleChange} required/>
+          Last Name:
+          <input
+            type="text"
+            name="last"
+            value={last}
+            onChange={handleChange}
+            required
+          />
         </div>
-        <br/>
+        <br />
         <button type="submit">Submit</button>
-        <br/>
-        <br/>
-
+        <br />
+        <br />
       </form>
       {submit ? (
-          
-           `Full Name: ${first} ${last}`
-          
-        ) : (
-          ""
-        )}
+        <p>
+          Full Name: {first} {last}
+        </p>
+      ) : (
+        ""
+      )}
     </>
   );
 }
